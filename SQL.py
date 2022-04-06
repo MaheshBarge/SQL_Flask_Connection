15ac28823ebcb427561f997aa8a6956410b0141f
MaheshBarge <64317290+MaheshBarge@users.noreply.github.com>
2022-04-06 11:07:50 +0530

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,22 +1,25 @@
+#import libraries
 from flask import Flask, request, jsonify
-
+import pymongo
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-
-
+# SQL connection configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'M@maxeffort.5'
-app.config['MYSQL_DB'] = 'cardataset'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'dataset'
 
 mysql = MySQL(app)
 
+#mongodb connection
+client = pymongo.MongoClient("mongodb+srv://mongodb:mongodb@cluster0.aa8cm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+
+#SQL request
 @app.route('/sql', methods = ['GET','POST'])
-
 def test2():
-
     if(request.method == ('POST')):
 
         cursor = mysql.connection.cursor()
@@ -26,5 +29,13 @@ def test2():
         result = cursor.fetchall()
         return jsonify(str(result))
 
+#mongodb request
+@app.route('/mongo', methods = ['GET'])
+def test3():
+    db = client['New']
+    coll = db['inneuron_collection']
+    return jsonify(str(list(coll.find().limit(5))))
+   
+#run flask app
 if __name__ == '__main__':
     app.run(port = 7000)
